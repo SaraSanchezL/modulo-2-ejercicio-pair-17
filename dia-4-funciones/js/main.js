bfb01a69687dcfa8c77096a1e04357d632ce258d
SaraSanchezL <sarasanchezlopez93@gmail.com>
2021-12-10 11:29:16 +0100

    add exercises day 4, 5 and 6

--- dia-4-funciones/js/main.js
+++ dia-4-funciones/js/main.js
@@ -4,13 +4,10 @@ const listLinks = document.querySelector(".js_datalist");
 const board_data = document.querySelector(".js-data");
 const menuDropdown = document.querySelector('.js-menudropdown');
 const input_search_desc = document.querySelector('.js-input-search');
-
-menuDropdown.classList.remove('collapsed');
-input_search_desc.value = 'materiales';
-
-const descrSearchText = input_search_desc.value;
+const newBookmark = document.querySelector(".js-data-actions");
 
 
+// bookmark 1
 const bmk_1_url =
   "https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-2-programando-la-web/javascript/2_1_intro_a_la_programacion";
 const bmk_1_desc = "JS en los materiales de Adalab";
@@ -18,9 +15,116 @@ const bmk_1_seen = "checked";
 const bmk_1_tags_1 = "javascript";
 const bmk_1_tags_2 = "html";
 
+
+// bookmark 2
+const bmk_2_url = 'https://thesmartcoder.dev/9-awesome-projects-you-can-build-with-vanilla-javascript/';
+const bmk_2_desc = 'Ideas de proyectos JS';
+const bmk_2_seen = 'checked';
+const bmk_2_tags_1 = 'javascript';
+const bmk_2_tags_2 = 'portfolio';
+
+
+// bookmark 3
+const bmk_3_url = 'https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-1-html-y-css/1_1_intro_a_la_web';
+const bmk_3_desc = 'HTML en los materiales de Adalab';
+const bmk_3_seen = '';
+const bmk_3_tags_1 = 'HTML';
+const bmk_3_tags_2 = 'CSS';
+
+
+// menuDropdown.classList.remove('collapsed');
+input_search_desc.value = 'materiales';
+const descrSearchText = input_search_desc.value;
+
+// funciones `displayTableView()` y `displayCardListView()`
+
+function displayTableView() {
+  board_data.classList.remove('listview');
+  board_data.classList.add("tableview");
+}
+// const displayTable = displayTableView();
+
+function displayCardListView() {
+  board_data.classList.remove('tableview');
+  board_data.classList.add("listview");
+}
+// const displayList = displayCardListView(); 
+
+
+// `showDropdownMenu()` y `hideDropdownMenu()`) para mostrar u ocultar el menú de visualización
+
+function showDropdownMenu() {
+  menuDropdown.classList.remove('collapsed');
+}
+// const showMenu = showDropdownMenu();
+
+function hideDropdownMenu() {
+  menuDropdown.classList.add('collapsed');
+}
+// const hideMenu = hideDropdownMenu();
+
+// `showAddForm()` que al ejecutarla nos muestre en la página el formulario para crear un nuevo bookmark
+
+function showAddForm() {
+  newBookmark.classList.remove("hidden");
+}
+// const showNewBookmark = showAddForm();
+
+
+// funcion renderBookmarkTags
+
+function renderBookmarkTags( tags_1, tags_2 ) {
+  if( tags_1 === '' && tags_2 == '' ) {
+    return `<p class="item__tags">No tiene</p>`;
+  }
+  else if( tags_2 === '' ) {
+    return `
+      <ul class="item__tags">
+        <li class="item__tag">${tags_1.toLowerCase()}</li>
+      </ul>`;
+  }
+  else {
+    return `
+      <ul class="item__tags">
+        <li class="item__tag">${tags_1.toLowerCase()}</li>
+        <li class="item__tag">${tags_2.toLowerCase()}</li>
+      </ul>`;
+  }
+}
+
+
+// funcion renderBookmark
+
+function renderBookmark( url, desc, lesson, tags_1, tags_2 ) {
+  const tagsHtml = renderBookmarkTags( tags_1, tags_2 );
+
+  let bookmarkHtml = 
+    `<li class="data__listitem">
+      <article class="data__item">
+        <p class="item__url">
+          <a href="${url}" target="_blank" rel="noopener noreferrer">
+          ${url.replace('https://','')}
+          </a>
+        </p>
+        <p class="item__seen">
+          <input type="checkbox" ${lesson} name="item_imp_2" id="item_imp_2">
+        </p>
+        <p class="item__desc">
+          ${desc}
+        </p>
+        ${tagsHtml}
+      </article>
+    </li>`;
+
+  return bookmarkHtml;
+}
+
+
+
+
 let html = '';
 
-if (bmk_1_desc.includes(descrSearchText)) {
+// if (bmk_1_desc.includes(descrSearchText)) {
   html = `<li class="data__listitem"> 
 <article class="data__item">
   <p class="item__url">
@@ -37,17 +141,10 @@ if (bmk_1_desc.includes(descrSearchText)) {
   </ul>
 </article>
 </li>`;
-}
+// }
 
 
-
-const bmk_2_url = 'https://thesmartcoder.dev/9-awesome-projects-you-can-build-with-vanilla-javascript/';
-const bmk_2_desc = 'Ideas de proyectos JS';
-const bmk_2_seen = 'checked';
-const bmk_2_tags_1 = 'javascript';
-const bmk_2_tags_2 = 'portfolio';
-
-if (bmk_2_desc.includes(descrSearchText)) {
+// if (bmk_2_desc.includes(descrSearchText)) {
 
   html += `<li class="data__listitem"> 
 <article class="data__item">
@@ -65,15 +162,9 @@ if (bmk_2_desc.includes(descrSearchText)) {
   </ul>
 </article>
 </li>`;
-}
+// }
 
-const bmk_3_url = 'https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-1-html-y-css/1_1_intro_a_la_web';
-const bmk_3_desc = 'HTML en los materiales de Adalab';
-const bmk_3_seen = '';
-const bmk_3_tags_1 = 'HTML';
-const bmk_3_tags_2 = 'CSS';
-
-if (bmk_3_desc.includes(descrSearchText)) {
+// if (bmk_3_desc.includes(descrSearchText)) {
 
   html += `<li class="data__listitem"> 
 <article class="data__item">
@@ -91,12 +182,14 @@ if (bmk_3_desc.includes(descrSearchText)) {
   </ul>
 </article>
 </li>`;
-}
+// }
 
 
 listLinks.innerHTML = html;
 
-/* DIA3.1: Prueba mostrar lista */
+
+/* DIA3.1: Prueba mostrar lista (sin y con funciones declaradas)
+
 
 if (board_data.classList.contains('tableview')) {
   board_data.classList.remove('tableview');
@@ -106,6 +199,11 @@ else if (board_data.classList.contains('listview')) {
   board_data.classList.remove('listview');
   board_data.classList.add('tableview');
 }
+*/
 
-
-
+if (board_data.classList.contains('tableview')) {
+  displayCardListView();
+}
+else if (board_data.classList.contains('listview')) {
+  displayTableView();
+}
--- dia-5-eventos/js/main.js
+++ dia-5-eventos/js/main.js
@@ -1,206 +1,93 @@
-'use strict';
+"use strict"
 
-/*              Sección de elementos que usamos en el HTML             */
-/*  -----------------------------------------------------------------  */
-
-const menuDropdown = document.querySelector('.js_menu_dropdown');
-const linkDropdown = document.querySelector('.js_link_dropdown');
-const sectionAdd = document.querySelector('.js_section_add');
-const buttonAdd = document.querySelector('.js_button_add');
-const boardData = document.querySelector('.js_boarddata');
-const listData = document.querySelector('.js_list_data');
+const listLinks = document.querySelector(".js_datalist");
+const board_data = document.querySelector(".js-data");
+const menuDropdown = document.querySelector('.js-menudropdown');
+const input_search_desc = document.querySelector('.js-input-search');
+const newBookmark = document.querySelector(".js-data-actions");
+const linkDropDown = document.querySelector(".js_link_dropdown");
 const buttonShowCardview = document.querySelector('.js_cardview_button');
 const buttonShowTable = document.querySelector('.js_table_button');
 
-/*    Sección de variables globales (que usamos en todo el fichero)    */
-/*  -----------------------------------------------------------------  */
 
-// Variables para el primer bookmark:
+// bookmark 1
+const bmk_1_url =
+  "https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-2-programando-la-web/javascript/2_1_intro_a_la_programacion";
+const bmk_1_desc = "JS en los materiales de Adalab";
+const bmk_1_seen = "checked";
+const bmk_1_tags_1 = "javascript";
+const bmk_1_tags_2 = "html";
 
-const bmk_1_url = 'https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-2-programando-la-web/javascript/2_1_intro_a_la_programacion';
-const bmk_1_desc = 'JS en los materiales de Adalab';
-const bmk_1_seen = 'checked title="Enlace leído"';
-const bmk_1_tags_1 = 'javascript';
-const bmk_1_tags_2 = '';
 
+// bookmark 2
 const bmk_2_url = 'https://thesmartcoder.dev/9-awesome-projects-you-can-build-with-vanilla-javascript/';
 const bmk_2_desc = 'Ideas de proyectos JS';
-const bmk_2_seen = 'checked title="Enlace leído"';
-const bmk_2_tags_1 = '';
-const bmk_2_tags_2 = '';
+const bmk_2_seen = 'checked';
+const bmk_2_tags_1 = 'javascript';
+const bmk_2_tags_2 = 'portfolio';
 
+
+// bookmark 3
 const bmk_3_url = 'https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-1-html-y-css/1_1_intro_a_la_web';
 const bmk_3_desc = 'HTML en los materiales de Adalab';
-const bmk_3_seen = 'title="Por leer"';
-const bmk_3_tags_1 = 'html';
-const bmk_3_tags_2 = 'css';
-
-// Nota: Uso _ en los nombres, pero por una razón académica
-// En JS se suele usar la notación camelCase para los identificadores
-// (identificador = nombre de variable)
+const bmk_3_seen = '';
+const bmk_3_tags_1 = 'HTML';
+const bmk_3_tags_2 = 'CSS';
 
 
+// menuDropdown.classList.remove('collapsed');
+input_search_desc.value = 'materiales';
+const descrSearchText = input_search_desc.value;
 
+// 4 FUNCIONES. `displayTableView()` y `displayCardListView()`
 
-
-/*                        Sección de funciones                         */
-/*  -----------------------------------------------------------------  */
-
-// Día 4: Funciones que generan el HTML de cada bookmark
-
-/**
- * Genera un código HTML para representar la lista de etiquetas de un bookmark,
- * teniendo en cuenta que alguna etiqueta (o las dos) puede estar vacía.
- * 
- * @param {string} tags_1 Nombre de la primera etiqueta de un bookmark
- * @param {string} tags_2 Nombre de la segunda etiqueta de un bookmark
- * @returns Un String con el HTML de la lista de etiquetas
- */
-
-function renderTags(tags_1, tags_2) {
-  let htmlTags = `
-    <ul class="item__tags">`;
-
-  if (tags_1 !== '') {
-    htmlTags += `
-      <li class="item__tag">${tags_1}</li>`;
-  }
-
-  if (tags_2 !== '') {
-    htmlTags += `
-      <li class="item__tag">${tags_2}</li>`;
-  }
-
-  htmlTags += `      
-    </ul>
-  `;
-
-  return htmlTags;
+function displayTableView() {
+  board_data.classList.remove('listview');
+  board_data.classList.add("tableview");
 }
+// const displayTable = displayTableView();
 
-/**
- * Genera el código HTML de un bookmark.
- * 
- * @param {String} url Dirección del bookmark
- * @param {String} desc Descripción del bookmark
- * @param {String} seen Atributos para el input-checkbox del HTML
- * @param {String} tags_1 Primera etiqueta
- * @param {String} tags_2 Segunda etiqueta
- * @returns Un String con el código HTML que representa el bookmark.
- */
-
-function renderBookmark(url, desc, seen, tags_1, tags_2) {
-  const htmlTags = renderTags(tags_1, tags_2);
-
-  const htmlBookmark = `
-    <li class="data__listitem">
-      <article class="data__item">
-        <p class="item__url">
-          <a href="${url}" target="_blank" rel="noopener noreferrer">
-            ${url}
-          </a>
-        </p>
-        <p class="item__seen">
-          <input type="checkbox" ${seen} name="item_seen_2" id="item_seen_2">
-        </p>
-        <p class="item__desc">${desc}</p>
-        ${htmlTags}
-      </article>
-    </li>
-    `;
-
-  return htmlBookmark;
+function displayCardListView() {
+  board_data.classList.remove('tableview');
+  board_data.classList.add("listview");
 }
-
-// Invocamos 3 veces a la función para generar el HTML de los bookmakrs
-// pero en cada invocación le pasamos como parámetro los datos del 
-// bookmark correspondiente.
-// El HTML que van generando, lo acumulamos en la variable html.
-
-let html = renderBookmark(bmk_1_url, bmk_1_desc, bmk_1_seen, bmk_1_tags_1, bmk_1_tags_2);
-html += renderBookmark(bmk_2_url, bmk_2_desc, bmk_2_seen, bmk_2_tags_1, bmk_2_tags_2);
-html += renderBookmark(bmk_3_url, bmk_3_desc, bmk_3_seen, bmk_3_tags_1, bmk_3_tags_2);
-
-// Ponemos todo el HTML generado como innerHTML del <ul> de la página.
-
-listData.innerHTML = html;
+// const displayList = displayCardListView(); 
 
 
-// Día 4: Funciones para cambiar la visualización de la lista de bookmarks.
+// `showDropdownMenu()` y `hideDropdownMenu()`) para mostrar u ocultar el menú de visualización
 
-/**
- * Cambia el modo de visualización de bookmarks a lista de tarjetas.
- */
-
-function setCardListView() {
-  boardData.classList.remove('tableview');
-  boardData.classList.add('listview');
-}
-
-/**
- * Cambia el modo de visualización de bookmarks tabla.
- */
-
-function setTableView() {
-  boardData.classList.remove('listview');
-  boardData.classList.add('tableview');
-}
-
-// Día 4: Funciones para mostrar y ocultar el menú dropdown
-
-/**
- * Muestra el menú colapsable
- */
-
-function showDropDownMenu() {
+function showDropdownMenu() {
   menuDropdown.classList.remove('collapsed');
 }
+// const showMenu = showDropdownMenu();
 
-/**
- * Oculta el menú colapsable
- */
-
-function hideDropDownMenu() {
+function hideDropdownMenu() {
   menuDropdown.classList.add('collapsed');
 }
+// const hideMenu = hideDropdownMenu();
 
-/**
- * Muestra u oculta el menú colapsable dependiendo de su estado actual.
- */
+// `showAddForm()` que al ejecutarla nos muestre en la página el formulario para crear un nuevo bookmark
 
+function showAddForm() {
+  newBookmark.classList.remove("hidden");
+}
+// const showNewBookmark = showAddForm();
+
+
+// 5. EVENTOS 
+// Hamburguesa. Ponemos clase en querySelector a <a>. Creamos una funcion que englobe las anteriores (show or hide) con un if..else en funcion de si posee la clase collapsed.
 function toggleDropDownMenu() {
 
   if (menuDropdown.classList.contains('collapsed')) {
-    showDropDownMenu();
+    showDropdownMenu();
   }
   else {
-    hideDropDownMenu();
+    hideDropdownMenu();
   }
-
-  // Lo anterior es lo mismo que usar:
-  //
-  // menuDropdown.classList.toggle('collapsed');
-
 }
+// Creamos el evento addListener a la const arriba declarada, y creamos otra funcion, en la que se evita el comportamiento por defecto y ademas se añade la funcion creada anteriormente.
 
-// Día 4: Función para mostrar el formulario de añadir bookmark
-
-/**
- * Muestra el formulario para añadir un nuevo bookmark
- */
-
-function showAddForm() {
-  sectionAdd.classList.remove('hidden');
-}
-
-
-
-/* Sección de eventos (addEventListener y funciones handler asociadas) */
-/*  -----------------------------------------------------------------  */
-
-// Día 5: Se escucha el click en el enlace del botón hamburguesa
-// para mostrar u ocultar el menú dropdown.
-linkDropdown.addEventListener('click', handleClickLinkDropdown);
+linkDropDown.addEventListener('click', handleClickLinkDropdown);
 
 function handleClickLinkDropdown(event) {
   event.preventDefault();
@@ -208,50 +95,164 @@ function handleClickLinkDropdown(event) {
 }
 
 
-// Día 5: Se escucha el click del botón "Vista tarjetas" del menú
-// dropdown y se cambia la visualización de los bookmarks a tarjetas,
-// además de resaltar ese botón.
-
-
+// List or Tableview. Añadimos en dos funciones las anteriores escritas para mostrar una vista u otra en funcion de la clase que contenga. En estas nuevas dos funciones, añadimos tambien preventDefault, y quitar y añadir la clase selected. 
 
 function handleClickShowCardview(event) {
   event.preventDefault();
-
-  setCardListView();
+  displayCardListView();
   buttonShowTable.classList.remove('selected');
   buttonShowCardview.classList.add('selected');
-
 }
 
-// Día 5: Se escucha el click del botón "Vista tabla" del menú
-// dropdown y se cambia la visualización de los bookmarks a tabla,
-// además de resaltar ese botón.
-
-function handleClickShowTable(event) {
+function handleClickShowTableview(event) {
   event.preventDefault();
-
-  setTableView();
+  displayTableView();
   buttonShowCardview.classList.remove('selected');
   buttonShowTable.classList.add('selected');
 }
 
-buttonShowCardview.addEventListener('click', handleClickShowCardview);
-buttonShowTable.addEventListener('click', handleClickShowTable);
+// Una vez hechas se llama al elemento, se añade addEventListener y los parametros. Click y la funcion que corresponda de las dos anteriores. 
+buttonShowCardview.addEventListener("click", handleClickShowCardview);
+buttonShowTable.addEventListener("click", handleClickShowTableview);
+
+// Formulario. En esta ya esta creada la función por lo que se añade un listener al elemento, y los parametros (click, y funcion a ejecutar)
+newBookmark.addEventListener("click", showAddForm);
 
 
 
 
-// Día 5: Se escucha el click en el botón "Nueva" para mostrar
-// el formulario de añadir una nueva tarjeta.
+/* funcion renderBookmarkTags
 
-buttonAdd.addEventListener('click', showAddForm);
-
-/*
-// Otra forma de hacerlo:
-
-function handleClickButtonAdd(event) {
-  showAddForm();
+function renderBookmarkTags( tags_1, tags_2 ) {
+  if( tags_1 === '' && tags_2 == '' ) {
+    return `<p class="item__tags">No tiene</p>`;
+  }
+  else if( tags_2 === '' ) {
+    return `
+      <ul class="item__tags">
+        <li class="item__tag">${tags_1.toLowerCase()}</li>
+      </ul>`;
+  }
+  else {
+    return `
+      <ul class="item__tags">
+        <li class="item__tag">${tags_1.toLowerCase()}</li>
+        <li class="item__tag">${tags_2.toLowerCase()}</li>
+      </ul>`;
+  }
 }
 
-buttonAdd.addEventListener('click', handleClickButtonAdd);
+
+// funcion renderBookmark
+
+function renderBookmark( url, desc, lesson, tags_1, tags_2 ) {
+  const tagsHtml = renderBookmarkTags( tags_1, tags_2 );
+
+  let bookmarkHtml = 
+    `<li class="data__listitem">
+      <article class="data__item">
+        <p class="item__url">
+          <a href="${url}" target="_blank" rel="noopener noreferrer">
+          ${url.replace('https://','')}
+          </a>
+        </p>
+        <p class="item__seen">
+          <input type="checkbox" ${lesson} name="item_imp_2" id="item_imp_2">
+        </p>
+        <p class="item__desc">
+          ${desc}
+        </p>
+        ${tagsHtml}
+      </article>
+    </li>`;
+
+  return bookmarkHtml;
+}
 */
+
+
+let html = '';
+
+// if (bmk_1_desc.includes(descrSearchText)) {
+  html = `<li class="data__listitem"> 
+<article class="data__item">
+  <p class="item__url">
+    <a href=${bmk_1_url.replace("https://", "")} target="_blank" rel="noopener noreferrer">
+    ${bmk_1_url.substring(8)} 
+    </a>
+  </p>
+  <p class="item__seen">
+    <input type="checkbox" ${bmk_1_seen} name="item_imp_2" id="item_imp_2">
+  </p>
+  <p class="item__desc">${bmk_1_desc}</p>
+  <ul class="item__tags">
+    <li class="item__tag">${bmk_1_tags_1.toLowerCase()}</li><li class="item__tag">${bmk_1_tags_2.toLowerCase()}</li>
+  </ul>
+</article>
+</li>`;
+// }
+
+
+// if (bmk_2_desc.includes(descrSearchText)) {
+
+  html += `<li class="data__listitem"> 
+<article class="data__item">
+  <p class="item__url">
+    <a href=${bmk_2_url} target="_blank" rel="noopener noreferrer">
+      ${bmk_2_url.replace("https://", "")}
+    </a>
+  </p>
+  <p class="item__seen">
+    <input type="checkbox" ${bmk_2_seen} name="item_imp_2" id="item_imp_2">
+  </p>
+  <p class="item__desc">${bmk_2_desc}</p>
+  <ul class="item__tags">
+    <li class="item__tag">${bmk_2_tags_1.toLowerCase()}</li><li class="item__tag">${bmk_1_tags_2.toLowerCase()}</li>
+  </ul>
+</article>
+</li>`;
+// }
+
+// if (bmk_3_desc.includes(descrSearchText)) {
+
+  html += `<li class="data__listitem"> 
+<article class="data__item">
+  <p class="item__url">
+    <a href=${bmk_3_url} target="_blank" rel="noopener noreferrer">
+     ${bmk_3_url.slice(8)}
+    </a>
+  </p>
+  <p class="item__seen">
+    <input type="checkbox" ${bmk_3_seen} name="item_imp_2" id="item_imp_2">
+  </p>
+  <p class="item__desc">${bmk_3_desc}</p>
+  <ul class="item__tags">
+    <li class="item__tag">${bmk_3_tags_1.toLowerCase()}</li><li class="item__tag">${bmk_3_tags_2.toLowerCase()}</li>
+  </ul>
+</article>
+</li>`;
+// }
+
+
+listLinks.innerHTML = html;
+
+
+/* DIA3.1: Prueba mostrar lista (sin y con funciones declaradas)
+
+
+if (board_data.classList.contains('tableview')) {
+  board_data.classList.remove('tableview');
+  board_data.classList.add('listview');
+}
+else if (board_data.classList.contains('listview')) {
+  board_data.classList.remove('listview');
+  board_data.classList.add('tableview');
+}
+*/
+
+if (board_data.classList.contains('tableview')) {
+  displayCardListView();
+}
+else if (board_data.classList.contains('listview')) {
+  displayTableView();
+}
